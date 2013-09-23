59a37d7f4eebd57e495f683418a333b752f9c0cd
David Eddlemon <DavidEddlemon@Hotmail.com>
2013-09-24 01:45:01 +0400

    Huh
    Change-Id: I14de48b7c554ca832812ce357b4add1a49a2ee2b

--- products/pa_vigor.mk
+++ products/pa_vigor.mk
@@ -1,36 +1,33 @@
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # Check for target product
 ifeq (pa_vigor,$(TARGET_PRODUCT))
 
 # Define PA bootanimation size
-PARANOID_BOOTANIMATION_NAME := XHDPI
+PARANOID_BOOTANIMATION_NAME := xhdpi
 
 # OVERLAY_TARGET adds overlay asset source
 OVERLAY_TARGET := pa_xhdpi
 
 # Build paprefs from sources
-PREFS_FROM_SOURCE ?= false
+PREFS_FROM_SOURCE := false
 
 # Include ParanoidAndroid common configuration
 include vendor/pa/config/pa_common.mk
 
-# inherit device configuration
-$(call inherit-product, device/htc/vigor/vigor.mk)
-
-# inherit common configuration
+# inherit from common msm8660
 -include device/htc/msm8660-common/BoardConfigCommon.mk
 
-# inherit vendor configuration
+# inherit from proprietary vendor
 -include vendor/htc/vigor/BoardConfigVendor.mk
+
+# Kernel
+-include kernel/htc/vigor-3.0/arch/arm/configs/vigor_aosp_defconfig
+product_device := vigor
+
+# define cpu variant
+TARGET_CPU_VARIANT := scorpion
+
+# inherit from vigor device
+$(call inherit-product, device/htc/vigor/vigor.mk)
 
 # Include CM extras
 EXTRA_CM_PACKAGES ?= true
